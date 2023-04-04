f22fa0fc9f47997e1ddc55bff11d2e249535d35b
Esther <e.mbanzabig@alustudent.com>
2023-04-04 14:14:06 +0200

    love coding

---
+++ log.py
@@ -0,0 +1,25 @@
+info = {'john': 12345, 'anissa': 00000, 'jacques': 20011, 'umutoni': 20008}
+def sign_up():
+    name = input('Enter your name: ')
+    password = int(input('Enter your password and should be 5 digits: '))
+    if name not in info or password != info[name]:
+        info[name] = password
+        print('Sign up successful.')
+        
+    elif name in info or password == info[name]:
+        print('Username or account existed')
+    
+    print(info)             
+def log_in():
+    name = input('Enter your name: ')
+    password = int(input('Enter your password and should be 5 digits: '))
+    
+    if name in info and password == info[name]:
+
+        print('Log in successful')
+        
+    else:
+        print('Log in denied.create an account or try again!')
+        sign_up()
+log_in()                    
+ 
